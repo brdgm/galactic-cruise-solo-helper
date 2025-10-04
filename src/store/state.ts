eb608460d442603de60286b4fcedd45ad941d20f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-04 15:49:08 +0200

    initial app structure

---
+++ src/store/state.ts
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia'
+import { name } from '@/../package.json'
+import DifficultyLevel from '@/services/enum/DifficultyLevel'
+import Player from '@/services/enum/Player'
+
+export const useStateStore = defineStore(`${name}.state`, {
+  state: () => {
+    return {
+      language: 'en',
+      baseFontSize: 1,
+      setup: {
+        difficultyLevel: DifficultyLevel.EASY
+      },
+      turns: []
+    } as State
+  },
+  actions: {
+    resetGame() {
+      this.turns = []
+      this.setup.initialCardDeck = undefined
+    },
+    storeTurn(turn : Turn) : void {
+      this.turns = this.turns.filter(item => item.turn < turn.turn)
+      this.turns.push(turn)
+    }
+  },
+  persist: true
+})
+
+export interface State {
+  language: string
+  baseFontSize: number
+  setup: Setup
+  turns: Turn[]
+}
+export interface Setup {
+  difficultyLevel: DifficultyLevel
+  initialCardDeck?: CardDeckPersistence
+  debugMode?: boolean
+}
+
+export interface Turn {
+  turn: number
+  round: number
+  player: Player
+  botPersistence?: BotPersistence
+}
+export interface BotPersistence {
+  cardDeck: CardDeckPersistence
+}
+export interface CardDeckPersistence {
+  pile: string[]
+  discard: string[]
+}
---
+++ src/views/SetupGame.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>{{t('setupGame.title')}}</h1>
+
+  <div class="instructions mt-4">
+    <p>...</p>
+  </div>
+
+  <button class="btn btn-primary btn-lg mt-4" @click="startGame()">
+    {{t('action.startGame')}}
+  </button>
+
+  <FooterButtons backButtonRouteTo="/setup" endGameButtonType="abortGame"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useStateStore } from '@/store/state'
+import FooterButtons from '@/components/structure/FooterButtons.vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  name: 'SetupGame',
+  components: {
+    FooterButtons
+  },
+  setup() {
+    const { t } = useI18n()
+    const state = useStateStore()
+    const router = useRouter()
+
+    return { t, state, router }
+  },
+  methods: {
+    startGame() : void {
+      this.router.push('/turn/1/player')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.instructions {
+  max-width: 1000px;
+  ol > li {
+    margin-top: 0.5rem;
+  }
+}
+</style>
