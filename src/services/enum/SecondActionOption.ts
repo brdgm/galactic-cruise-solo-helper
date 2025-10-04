b5e3d2316cc04c4978449d859230d79099518a23
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-04 17:44:18 +0200

    difficulty level

---
+++ src/services/enum/SecondActionOption.ts
@@ -0,0 +1,9 @@
+/**
+ * Second Action Option
+ */
+enum SecondActionOption {
+  SAME_LOCATION = 'same-location',
+  ADJACENT_CONNECTED_LOCATION = 'adjacent-connected-location',
+  ANY_CONNECTED_LOCATION = 'any-connected-location'
+}
+export default SecondActionOption
