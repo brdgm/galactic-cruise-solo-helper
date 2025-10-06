27487909c9cdbb8364a6a3c22db40ad063813053
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-06 18:30:09 +0200

    expert

--- src/components/turn/action/ActionHireExpertWorkerBot.vue
+++ src/components/turn/action/ActionHireExpertWorkerBot.vue
@@ -1,10 +1,16 @@
 <template>
-  <ActionBox :instructionTitle="t(`rules.bot.action.${action.action}.title`)" :modalSizeLg="true">
+  <ActionBox :instructionTitle="t(`rules.bot.action.${action.action}.title`)">
     <template #action>
       <AppIcon type="action" :name="action.action" extension="svg" class="icon"/>
     </template>
     <template #instruction>
-      <p v-html="t(`rules.bot.action.${action.action}.instructions`)"></p>
+      <ul>
+        <li v-html="t(`rules.bot.action.${action.action}.firstExpert`)"></li>
+        <li v-html="t(`rules.bot.action.${action.action}.addToBreakRoom`)"></li>
+        <li v-html="t(`rules.bot.action.${action.action}.secondExpert`)"></li>
+      </ul>
+      <p v-if="isEasyDifficultyLevel" class="fw-bold" v-html="t(`rules.bot.action.${action.action}.easyDifficulty`)"></p>
+      <p v-else class="fw-bold" v-html="t(`rules.bot.action.${action.action}.normalDifficulty`)"></p>
     </template>
   </ActionBox>
 </template>
@@ -16,6 +22,8 @@ import NavigationState from '@/util/NavigationState'
 import { CardAction } from '@/services/Card'
 import ActionBox from '@/components/structure/ActionBox.vue'
 import AppIcon from '@/components/structure/AppIcon.vue'
+import { useStateStore } from '@/store/state'
+import DifficultyLevel from '@/services/enum/DifficultyLevel'
 
 export default defineComponent({
   name: 'ActionHireExpertWorkerBot',
@@ -38,7 +46,13 @@ export default defineComponent({
   },
   setup() {
     const { t } = useI18n()
-    return { t }
+    const state = useStateStore()
+    return { t, state }
+  },
+  computed: {
+    isEasyDifficultyLevel() : boolean {
+      return this.state.setup.difficultyLevel == DifficultyLevel.EASY
+    }
   }
 })
 </script>
